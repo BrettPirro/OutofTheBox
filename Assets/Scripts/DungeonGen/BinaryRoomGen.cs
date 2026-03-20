c7070b18d40da9887cb88eef00af6237cb05b813
MrAporia <114781092+BrettPirro@users.noreply.github.com>
2026-03-19 22:55:18 -0400

    corridor increase

--- Assets/Scripts/DungeonGen/BinaryRoomGen.cs
+++ Assets/Scripts/DungeonGen/BinaryRoomGen.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BinaryRoomGen : DungeonGenSimple
 {
@@ -29,8 +32,117 @@ public class BinaryRoomGen : DungeonGenSimple
 
         floor = CreateSimpleRoom(roomList);
 
+        List<Vector2Int> roomCenters = new List<Vector2Int>();
+
+        foreach (var room in roomList)
+        {
+            roomCenters.Add((Vector2Int)Vector3Int.RoundToInt(room.center));
+        }
+
+        HashSet<Vector2Int> corridors = ConnectRooms(roomCenters);
+
+      
+
+
+
+        floor.UnionWith(corridors);
+
         tileMapVis.PaintFloorTiles(floor);
         WallGen.CreateWalls(floor, tileMapVis);
+    }
+
+
+    
+    private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)
+    {
+        HashSet<Vector2Int> corridors = new HashSet<Vector2Int>();
+        var currentRoomCenter = roomCenters[Random.Range(0, roomCenters.Count)];
+        roomCenters.Remove(currentRoomCenter);
+
+        while (roomCenters.Count>0)
+        {
+            Vector2Int closet = FindClosetPointTo(currentRoomCenter,roomCenters);
+            roomCenters.Remove(closet);
+            HashSet<Vector2Int> newCorridor = CreateCorridor(currentRoomCenter, closet);
+            currentRoomCenter = closet;
+            corridors.UnionWith(newCorridor);
+        }
+        return corridors;
+
+    }
+
+
+
+    private HashSet<Vector2Int> CreateCorridor(Vector2Int currentRoomCenter, Vector2Int closet)
+    {
+        HashSet<Vector2Int> corridor = new HashSet<Vector2Int>();
+        var pos = currentRoomCenter;
+
+
+
+        corridor.Add(pos);
+
+        while (pos.y != closet.y)
+        {
+            Vector2Int direction;
+
+            if (closet.y > pos.y)
+                direction = Vector2Int.up;
+            else
+                direction = Vector2Int.down;
+
+            pos += direction;
+
+            corridor.Add(pos);
+
+
+            Vector2Int perp = new Vector2Int(-direction.y, direction.x);
+
+            corridor.Add(pos + perp);      
+            corridor.Add(pos - perp);
+        }
+
+        while (pos.x != closet.x)
+        {
+            Vector2Int direction;
+
+            if (closet.x > pos.x)
+                direction = Vector2Int.right;
+            else
+                direction = Vector2Int.left;
+
+            pos += direction;
+
+            corridor.Add(pos);
+
+            Vector2Int perp = new Vector2Int(-direction.y, direction.x);
+
+            corridor.Add(pos + perp);     
+            corridor.Add(pos - perp);
+        }
+        return corridor;
+    }
+
+
+
+
+    private Vector2Int FindClosetPointTo(Vector2Int currentRoomCenter, List<Vector2Int> roomCenters)
+    {
+        Vector2Int closet = Vector2Int.zero;
+        float length = float.MaxValue;
+
+        foreach (var pos in roomCenters)
+        {
+            float currentDist = Vector2.Distance(pos, currentRoomCenter);
+
+            if (currentDist<length) 
+            { 
+                length = currentDist;
+                closet = pos;
+            }
+        }
+        return closet;
+
     }
 
     private HashSet<Vector2Int> CreateSimpleRoom(List<BoundsInt> roomList)
