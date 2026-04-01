2f83da20efbcdc3cce98294109643f6fb34a83bd
MrAporia <114781092+BrettPirro@users.noreply.github.com>
2026-03-31 20:16:39 -0400

    last fix

--- Assets/Scripts/ETC/BoxEndLevelObj.cs
+++ Assets/Scripts/ETC/BoxEndLevelObj.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using static UnityEditor.Recorder.OutputPath;
+
 
 public class BoxEndLevelObj : MonoBehaviour
 {
